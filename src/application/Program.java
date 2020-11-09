ac3f5483914380fc6d69d3de8a085bdba0203516
Fabio Celis <fabiohcelis@gmail.com>
2020-11-09 20:27:24 +0000

    Primeiro Commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import enteties.Reservation;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,5 +17,24 @@ public class Program {
         int roomNumber =sc.nextInt();
         System.out.print("Check-in date (dd/MM/yyyy): ");
         Date checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
+
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+
+            reservation.updateDates(checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+        }
     }
 }
